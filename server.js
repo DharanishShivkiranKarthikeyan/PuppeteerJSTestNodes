90fa2330ebf8d98eb8e772a6633354795d8d54cd
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-05-14 09:19:38 -0400

    first

---
+++ server.js
@@ -0,0 +1,37 @@
+
+
+const puppeteer = require("puppeteer");
+// Launch the browser and open a new blank page
+
+async function main(){
+    var browser = await puppeteer.launch({args: ['--no-sandbox'],timeout:0});
+    for(let i = 0; i<5;i++){
+        await signupNode(browser);
+    }
+    setTimeout(()=>{
+        return;
+    },2147483646)
+}
+
+
+
+async function signupNode(browser){
+    page = await browser.newPage();
+    const minLoadingTime = new Promise(resolve => setTimeout(resolve, 2750));
+
+    
+    // Navigate the page to a URL.
+    await page.goto('https://dharanishshivkirankarthikeyan.github.io/datasharingApp/signup.html');
+    await minLoadingTime
+
+    // Set screen size.
+
+    var element = await page.waitForSelector('::-p-xpath(//*[@id="signupNodeButton"])');
+   await  minLoadingTime
+    await element.click();
+    minLoadingTime
+    await await element.dispose();
+
+}
+
+main();
